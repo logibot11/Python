50e3890f606480c2ec8defab508cac82ed066355
Jaroslaw Krefft <tech@krefft.pl>
2026-06-22 15:23:37 +0200

    Dokonanie zmiany w logice - powiadomienie gospodarza

--- modul1_podstawy/variable_types/boolean_type.py
+++ modul1_podstawy/variable_types/boolean_type.py
@@ -4,11 +4,23 @@
 # Nazwa i wersja programu: boolean_type.py
 # W kodzie jest zaprezentowany sposób użycia zmiennej boolean na przykładzie logiki czujki ruchu zintegrowanej z naświetlaczem
 
-detect_activity = False
+
+
+detect_activity_garden = True # wykrycie ruchu w ogrodzie
+detect_activity_garage = False # wykrycie ruchu na podjeździe do garażu
 is_night = True
 
+garden_sensor = False #stan czujnika ruchu w ogrodzie 
+garage_sensor = False # stan czujnika ruchu na podjeździe do garażu
 
-if detect_activity and is_night:
+
+if detect_activity_garden and is_night:
     print("Włącz oświetlenie ogrodowe.")
-else:
-    print("Brak reakcji systemu oświetlenia")
+    garden_sensor = True
+
+if detect_activity_garage and is_night:
+    print("Włącz oświetlenie na podjeździe garażowym.")
+    garage_sensor = True
+
+if garden_sensor or garage_sensor:
+    print("Uwaga pojawił się obiekt ruchomy na posesji!")

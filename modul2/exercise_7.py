5406509fbee419e89504476b5b6c74236d72d020
Jaroslaw Krefft <tech@krefft.pl>
2026-07-08 13:31:52 +0200

    Dodanie pliku exercise_7.py

---
+++ modul2/exercise_7.py
@@ -0,0 +1,19 @@
+# Autor uczący się: Jarosław Krefft
+# Data utworzenia: 08.07.2026
+# Nazwa i wersja programu: exercise_7.py
+# Wytyczne Kamila: Napisz program, który policzy pole prostokata (użytkownik musi podać długości boków)
+
+a = 0
+b = 0
+
+print("W celu obliczenia pola prostokata zostaną pobrane od użytkownika długosci boków a i b\n")
+
+a = int(input("Podaj długość boku a prostokąta: "))
+b = int(input("Podaj długość boku b prostokata: "))
+
+pole  = a * b
+
+print(f"\nPole prostokąta wynosi {pole} \n")
+
+
+

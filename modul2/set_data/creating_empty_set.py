d448cdbccb5aa5bde232d7c71d5a839d695a5aa6
Jaroslaw Krefft <tech@krefft.pl>
2026-07-12 10:38:42 +0200

    creating_empty_set.py

---
+++ modul2/set_data/creating_empty_set.py
@@ -0,0 +1,10 @@
+# Autor uczący się: Jarosław Krefft
+# Data utworzenia: 12.07.2026
+# Nazwa i wersja programu: creating_empty_set.py
+# W kodzie są pokazane różne operacje na strukturze set
+
+names = {'Kazik', 'Ania', 'Ewelina', 'Nela'} # utworzenie setu z elementami
+print(names)
+
+names = set() # utworzenie pustego setu
+print(names)
